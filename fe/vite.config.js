bb266a5fd00d40b4d45de08fdedc6f7cd41cdc13
sonnu <sonle.021004@gmail.com>
2025-02-07 23:26:59 +0700

    xong-home

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -4,22 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  theme: {
-    extend: {
-      keyframes: {
-        lineRun: {
-          '0%': { transform: 'translateX(-100%)' },
-          '100%': { transform: 'translateX(100%)' },
-        },
-      },
-      animation: {
-        'line-run': 'lineRun 2s linear infinite',
-      },
-      colors: {
-        aqua: '#00FFFF',
-      },
-    },
-  },
   plugins: [
     react(),
     tailwindcss(),
